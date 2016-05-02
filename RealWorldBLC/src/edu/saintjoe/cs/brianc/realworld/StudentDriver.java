47b438ea1293180f83549bdd7bed661d779bc393
Brian Capouch <brianc@palaver.net>
2016-05-02 02:55:09 -0400

    Change real students to pretend ones

--- RealWorldBLC/src/edu/saintjoe/cs/brianc/realworld/StudentDriver.java
+++ RealWorldBLC/src/edu/saintjoe/cs/brianc/realworld/StudentDriver.java
@@ -8,24 +8,16 @@ public class StudentDriver {
 		int studentCount = 0;
 		
 		Course cmp112 = new Course("CMP112A", "B. Capouch", "Core 125");
-		Course cmp113= new Course("CMP113", "Core 115");
 		
-		Student nextStudent = new Student("Jared Alt", "M", 1, 19);
+		Student nextStudent = new Student("Wiley Abbott", "M", 1, 18);
 		// To put data into our private array, we had to write a mutator
 		//  We pass it a filled-in Student record, and our "counter" which is studentCount
 		cmp112.setStudent(nextStudent, (studentCount++));
-		// cmp112.students[studentCount++] = nextStudent;
-		nextStudent = new Student("Jude Brace", "M", 2, 20);
-		// cmp112.students[studentCount++] = nextStudent;
+		nextStudent = new Student("Jim Ballew", "M", 2, 19);
 		cmp112.setStudent(nextStudent, studentCount++);
-		nextStudent = new Student("Zach Bobos", "X", 1, 19);
-		// cmp112.students[studentCount++] = nextStudent;
+		nextStudent = new Student("Christine Black", "F", 1, 20);
 		cmp112.setStudent(nextStudent, studentCount++);
 		// Add YOURSELF!!!
-		nextStudent = new Student("Ryan Postma", "M", 3, 20);
-		cmp112.setStudent(nextStudent, studentCount++);
-		nextStudent = new Student("Chandler Gull", "M", 1, 19);
-		cmp112.setStudent(nextStudent, studentCount++);
 		System.out.println(cmp112.toString()); 
 
 	}
